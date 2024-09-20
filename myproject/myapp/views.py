0f8ea64593bb0a76b9bcfd2022b6d988a203d688
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-20 15:10:41 -0700

    Return render

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def about(request):
-    render(request, "about.html")
+    about_content = {'about' : "hello"}
+    return render(request, "about.html", about_content)
